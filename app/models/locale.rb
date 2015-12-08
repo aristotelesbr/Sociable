7ad88b5cac9dcafb53f5468bca936acc08151d1e
Ruby <ruby@localhost.localdomain>
2015-12-08 09:03:10 -0200

    Add geocoder for get latitude and longitude

--- app/models/locale.rb
+++ app/models/locale.rb
@@ -1,3 +1,19 @@
 class Locale < ActiveRecord::Base
   belongs_to :category
+   # Geocoder
+  geocoded_by :location do |obj, results|
+    if geo = results.first
+        # populate your model
+        obj.latitude = geo.latitude
+        obj.longitude = geo.longitude
+      else
+      obj.street = nil
+    end
+  end
+  before_validation :geocode, :if => :street_changed?
+
+  # Joins address to consult coords
+  def location
+  	[state, city, street, number].compact.join(', ')
+  end
 end
